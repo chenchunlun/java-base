0331a2c3d685099b05e32cde201718ecfa3179df
chenchunlun <1310024594@qq.com>
2016-05-15 07:57:46 +0800

    缓存字节流 BufferedInputStream BufferedOutputStream 以及缓存输入输出字节流

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import io.ByteStream;
 import io.CharStream;
+import io.ProcessStream;
 
 public class Main {
 
     public static void main(String[] args) {
         // ByteStream.test();
-        CharStream.test();
+        //CharStream.test();
+        ProcessStream.test();
     }
 }
---
+++ src/io/ProcessStream.java
@@ -0,0 +1,88 @@
+package io;
+
+import java.io.*;
+
+/**
+ * Created by hp on 2016/5/15 0015.
+ */
+public class ProcessStream {
+    public static void test(){
+       // bufferdInputStream();
+        bufferedReader();
+    }
+
+
+    public static void bufferedReader(){
+        File src = new File("F:/ideaProjects/java-base/src/io/ProcessStream.java");
+        File dest = new File("F:/ProcessStream.java");
+        BufferedReader read = null;
+        BufferedWriter writer = null;
+        try {
+            read = new BufferedReader(new FileReader(src));
+            writer = new BufferedWriter(new FileWriter(dest));
+            String str = null;
+            while (null!=(str=read.readLine())){
+                writer.write(str);
+                writer.newLine();
+            }
+            writer.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(writer!=null){
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(read!=null){
+            }
+        }
+    }
+
+    /**
+     * 缓存字节流
+     */
+    public static void bufferdInputStream(){
+        File src = new File("F:/ideaProjects/java-base/src/io/ProcessStream.java");
+        File dest = new File("F:/ProcessStream.java");
+        if(dest.exists()){
+            dest.delete();
+        }
+
+        BufferedInputStream inputStream = null;
+        BufferedOutputStream outputStream = null;
+        try {
+            dest.createNewFile();
+            inputStream = new BufferedInputStream(new FileInputStream(src));
+            outputStream =new BufferedOutputStream( new FileOutputStream(dest,true));
+            byte[] bytes = new byte[1024];
+            int len = -1;
+            while(-1!=(len=inputStream.read(bytes))){
+                outputStream.write(bytes,0,len);
+            }
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
