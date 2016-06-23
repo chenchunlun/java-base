099e4fb8f6191ea967fb24bc69cd661e7fef2980
陈春伦 <zhouchongxing@qianmi.com>
2016-06-23 18:04:24 +0800

    nio

--- src/nio/Test.java
+++ src/nio/Test.java
@@ -1,5 +1,9 @@
 package nio;
 
+import java.io.BufferedReader;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+
 /**
  * Created by Administrator on 2016-6-22.
  */
@@ -7,6 +11,12 @@ public class Test {
     public static void test(){
         //ReadTest.test();
         //WriteTest.test();
-        FileCopyTest.test();
+        //FileCopyTest.test();
+
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        System.out.println(buffer.capacity());
+        System.out.println(buffer.limit());
+        System.out.println(buffer.position());
+        System.out.println(buffer.isReadOnly());
     }
 }
